272e309c9c3eb70d5131e4bbbcd8f734c3be3da5
Kennie <Kenneth Chan>
2022-11-29 16:25:42 -0500

    able to delete/update URLs now

--- express_server.js
+++ express_server.js
@@ -61,6 +61,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`)
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.editURL;
+  res.redirect(`/urls`)
+})
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls")
