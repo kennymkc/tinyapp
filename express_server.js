b224da44f901d4829248b6785637df71feb73a7b
Kennie <Kenneth Chan>
2022-12-03 14:04:33 -0500

    clean up code for better functionality

--- express_server.js
+++ express_server.js
@@ -35,61 +35,70 @@ app.get("/urls", (req, res) => {
   const userURLs = urlForUser(userID, urlDatabase);
   if (!userID) {
     res.send('Please Login or Register');
+  } else {
+    const templateVars = {
+      user: users[userID],
+      urls: userURLs
+    };
+    res.render("urls_index", templateVars);
   }
-  const templateVars = {
-    user: users[userID],
-    urls: userURLs
-  };
-  res.render("urls_index", templateVars);
 });
 
 app.get("/register", (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
   if (req.session.user_id) {
-    return res.redirect("/urls");
+    res.redirect("/urls");
+  } else {
+    res.render("urls_register", templateVars);
   }
-  res.render("urls_register", templateVars);
 });
 
 app.get("/login", (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
   if (req.session.user_id) {
-    return res.redirect("/urls");
+    res.redirect("/urls");
+  } else {
+    res.render("urls_login", templateVars);
   }
-  res.render("urls_login", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.session.user_id] };
   if (!req.session.user_id) {
-    return res.redirect("/login");
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
   }
-  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   const database = urlDatabase[req.params.id];
+  if (!database) {
+    return res.send("URL Does Not Exist");
+  }
   if (!userID) {
     return res.send("Please Login to View");
   }
   if (userID !== database.userID) {
-    return res.send("Need Permission to View");
+    res.send("Need Permission to View");
+  } else {
+    const templateVars = {
+      user: users[userID],
+      id: req.params.id, longURL: urlDatabase[req.params.id].longURL
+    }
+    res.render("urls_show", templateVars);
   }
-  const templateVars = {
-    user: users[userID],
-    id: req.params.id, longURL: urlDatabase[req.params.id].longURL
-  };
-  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id].longURL;
+  const longURL = urlDatabase[req.params.id];
   if (!longURL) {
     res.status(404);
-    return res.send('Invalid ID');
+    res.send('Invalid ID');
+  } else {
+    res.redirect(longURL.longURL);
   }
-  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
@@ -99,9 +108,10 @@ app.post("/urls", (req, res) => {
       longURL: req.body.longURL,
       userID: req.session.user_id
     };
-    return res.redirect(`/urls/${id}`);
+    res.redirect(`/urls/${id}`);
+  } else {
+    res.send('Please log in to continue.');
   }
-  res.send('Please log in to continue.');
 });
 
 app.post("/login", (req, res) => {
@@ -113,7 +123,7 @@ app.post("/login", (req, res) => {
   }
   if (user) {
     if (bcrypt.compareSync(req.body.password, user.password)) {
-      res.cookie('user_id', user.id);
+      req.session.user_id = user.id;
       res.redirect("/urls");
     } else {
       res.status(403);
@@ -132,28 +142,29 @@ app.post("/register", (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 10);
-  if (email === '' || password === '') {
+  if (email === '' || req.body.password === '') {
     res.status(400);
-    res.send('400 Bad Request Cannot Leave Fields Empty');
+    return res.send('400 Bad Request Cannot Leave Fields Empty');
   }
   if (getUserByEmail(email, users)) {
     res.status(400);
     res.send('400 Bad Request Re-Enter Email');
+  } else {
+    users[id] = {
+      id,
+      email,
+      password
+    };
+    req.session.user_id = id;
+    res.redirect("/urls");
   }
-  users[id] = {
-    id,
-    email,
-    password
-  };
-  req.session.user_id = id;
-  res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {
   const userID = req.session.user_id;
   const database = urlDatabase[req.params.id];
   if (userID && userID === database.userID) {
-    urlDatabase[req.params.id] = req.body.editURL;
+    urlDatabase[req.params.id].longURL = req.body.editURL;
     res.redirect("/urls");
   } else {
     res.send('Permission Not Granted');
